6766e48093929ecab5bcc52497df43e8c934b056
BlackBox <1647010108@qq.com>
2023-04-14 10:16:29 +0800

    feat(监视文件系统): 添加启动项功能
    仅支持window 添加到启动项目录
    
    添加完毕
    测试成功

--- 监视文件系统/cache.py
+++ 监视文件系统/cache.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import ctypes
+import sys
+
+from pytz import unicode
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from shutil import copyfile, copytree
@@ -13,10 +19,14 @@ global dirs
 global dirStart
 global start_configs
 global stop_configs
+config_num: int
 
 
-# 可视化选择文件
 def get_local_file():
+    """
+     可视化选择文件夹
+    @return:
+    """
     root = tk.Tk()
     root.withdraw()
 
@@ -27,7 +37,7 @@ def get_local_file():
     return file_path
 
 
-def read_config(num: int):
+def read_config(num: int = 1):
     global start_configs
     global stop_configs
     """
@@ -37,18 +47,19 @@ def read_config(num: int):
     #  实例化configParser对象
     config = configparser.ConfigParser()
     # -read读取ini文件
-    config.read('config.ini', encoding='UTF-8')
-    start_dirs = ["startDirs" + str(i) for i in range(num)]
-    stop_dirs = ["stopDirs" + str(i) for i in range(num)]
 
-    start_configs = [str(config.get('dirs', i)) for i in start_dirs]
-    stop_configs = [str(config.get('dirs', i)) for i in stop_dirs]
+    config.read('config.ini', encoding='UTF-8')
+    num = int(config.get("config", "size"))
+    start_dirs = ["start" + str(i) for i in range(num)]
+    stop_dirs = ["stop" + str(i) for i in range(num)]
+
+    start_configs = [str(config.get('config', i)) for i in start_dirs]
+    stop_configs = [str(config.get('config', i)) for i in stop_dirs]
 
     return
 
 
 class MyHandler(FileSystemEventHandler):
-
 
     def on_modified(self, event):
         return
@@ -73,13 +84,18 @@ class MyHandler(FileSystemEventHandler):
             print("it's a special file(socket,FIFO,device file)")
 
 
-def start_(xx: int):
+def start_(xx: int = 1):
+    """
+        启动器
+    @param xx:
+    """
     global dirStart
     global dirs
     print(start_configs)
     print(stop_configs)
 
     for i in range(xx):
+
         dirStart = start_configs[i]
         dirs = stop_configs[i]
         is_dirs = True
@@ -135,13 +151,55 @@ def start_num1(number: int, string: str):
         time.sleep(2)
 
 
+def is_admin():
+    """
+        判断是否为管理员权限
+    @return:
+    """
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
 if __name__ == "__main__":
-    config_num = int(input("请输入启动配置中的数量\n"))
-    read_config(config_num)
-    # s1 = Thread(target=start_num1, args=(10, '第' + str(1) + '个线程'))
-    # s2 = Thread(target=start_num1, args=(10, '第' + str(2) + '个线程'))
-    # s3 = Thread(target=start_num1, args=(10, '第' + str(3) + '个线程'))
-    # s1.start()
-    # s2.start()
-    # s3.start()
-    start_(config_num)
+    #  实例化configParser对象
+    config = configparser.ConfigParser()
+    # 如果文件不存在配置config.ini
+    if not os.path.exists('config.ini'):
+        config_num = int(input("请输入启动配置中的数量\n"))
+        file = open("config.ini", 'w', encoding='UTF-8')
+        config.read('config.ini', encoding='UTF-8')
+        config.add_section("config")
+        config.set("config", "size", str(config_num))
+        for i in range(config_num):
+            print("请选择第%d个起始文件夹" % i)
+            temp_dir = get_local_file()
+            config.set("config", "start" + str(i), temp_dir)
+            print("请选择第%d个结束文件夹" % i)
+            temp_dir = get_local_file()
+            config.set("config", "stop" + str(i), temp_dir)
+        config.write(file)
+        file.close()
+
+    name = os.path.basename(__file__)
+    print(name)
+
+    if not os.path.exists("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\" + name):
+        if input("是否加入启动目录 (y) \n") == 'y':
+            if is_admin():
+                # 将要运行的代码加到这里
+                copyfile("cache8.0.exe",
+                         "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\" + name)
+                copyfile("config.ini", "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\config.ini")
+            else:
+                if sys.version_info[0] == 3:
+                    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+                else:  # in python2.x
+                    ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(__file__),
+                                                        None, 1)
+                exit()
+
+    read_config()
+
+    start_()
